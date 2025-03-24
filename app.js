ee4dc6415339125f86f748d05b644fbcf4c3ba69
Abigail Chelanagt <abigailchelah@gmail.com>
2025-03-23 22:51:05 -0700

    Add app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetchMovieDetails(1);
+    fetchFirstMovie();
     fetchAllMovies();
 });
 
-function fetchMovieDetails(id) {
-    fetch(`http://localhost:3000/films/${id}`)
+function fetchFirstMovie() {
+    fetch('http://localhost:3000/films/1')
         .then(response => response.json())
         .then(movie => {
             displayMovieDetails(movie);
@@ -30,5 +30,57 @@ function displayMovieDetails(movie) {
     const buyTicketButton = document.getElementById('buy-ticket');
     buyTicketButton.disabled = availableTickets <= 0;
     buyTicketButton.innerText = availableTickets > 0 ? 'Buy Ticket' : 'Sold Out';
-    buyTicketButton.onclick = () => buyTicket(movie.id, availableTickets);
+
+    buyTicketButton.onclick = () => {
+        if (availableTickets > 0) {
+            buyTicket(movie.id, availableTickets);
+        }
+    };
+}
+
+function displayMoviesList(movies) {
+    const filmsList = document.getElementById('films');
+    filmsList.innerHTML = '';
+    movies.forEach(movie => {
+        const li = document.createElement('li');
+        li.className = 'film item';
+        li.innerText = movie.title;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete';
+        deleteButton.onclick = () => deleteFilm(movie.id);
+        li.appendChild(deleteButton);
+
+        filmsList.appendChild(li);
+    });
+}
+
+function buyTicket(movieId, availableTickets) {
+    fetch(`http://localhost:3000/films/${movieId}`)
+        .then(response => response.json())
+        .then(movie => {
+            const newTicketsSold = movie.tickets_sold + 1;
+
+            fetch(`http://localhost:3000/films/${movieId}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ tickets_sold: newTicketsSold })
+            })
+            .then(response => response.json())
+            .then(updatedMovie => {
+                displayMovieDetails(updatedMovie);
+                fetchAllMovies();
+            });
+        });
+}
+
+function deleteFilm(movieId) {
+    fetch(`http://localhost:3000/films/${movieId}`, {
+        method: 'DELETE'
+    })
+    .then(() => {
+        fetchAllMovies();
+    });
 }
